94bb17e14695c59856c081ef165aa21c7509e04b
NathanRayM <143342815+NathanRayM@users.noreply.github.com>
2025-11-17 17:42:11 -0500

    added error to submit button

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const subContainer = document.querySelector(".submission");
 const mainContainer = document.querySelector(".container");
 const message = document.querySelector(".submission__message--value");
 
-submitBtn.disabled = true;
-
 let selectedRating = null;
+
+submitBtn.setAttribute("aria-disabled", "true");
 
 /* === Button rating active & store value === */
 buttons.forEach((button) => {
@@ -15,7 +15,7 @@ buttons.forEach((button) => {
     buttons.forEach((btn) => btn.classList.remove("active"));
     button.classList.add("active");
     selectedRating = button.textContent;
-    submitBtn.disabled = false;
+    submitBtn.setAttribute("aria-disabled", "false");
   });
 });
 
@@ -25,5 +25,7 @@ submitBtn.addEventListener("click", () => {
     message.textContent = selectedRating;
     mainContainer.hidden = true;
     subContainer.hidden = false;
+  } else {
+    alert("Please select a rating!");
   }
 });
